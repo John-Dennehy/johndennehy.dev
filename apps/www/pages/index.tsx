59446341b76708c22db4af016a6249025ada0b5c
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-02-10 16:28:55 +0000

    chore: configures www app to use tailwind from ui package

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -1,10 +1,17 @@
-import { Button } from "ui";
+import { Button, Card } from "ui";
 
-export default function Web() {
+export default function Home() {
   return (
     <div>
-      <h1>WWW</h1>
+      <h1 className="text-3xl text-teal-700 underline">WWW</h1>
+      <p className="text-gray-700">Welcome to www!</p>
       <Button />
+      <Card
+        cta="Click me"
+        href="https://www.google.com"
+        title="Card title" 
+      />
+       
     </div>
   );
 }
