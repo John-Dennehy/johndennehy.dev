a9320e7bd3508e1d1ede6f4bdd4daac9cda7e0fb
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-07 20:22:34 +0000

    Update font imports in layout component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import "@/styles/globals1.scss";
 import { Inter } from "next/font/google";
 import { Caveat } from "next/font/google";
 import { Outfit } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+
 
 import { ClerkProvider } from "@clerk/nextjs";
 import DefaultLayout from "@/components/Layout";
 import { cn } from "../lib/utils";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
 const caveat = Caveat({ subsets: ["latin"], variable: "--font-caveat" });
 
@@ -24,18 +26,22 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <ClerkProvider>
-      <html
-        lang="en"
-        className={cn(
-          "h-screen-dvh w-screen-dvw scrollbar-gutter overflow-x-hidden",
-          `${inter.variable} ${caveat.variable} ${outfit.variable}`
-        )}
-      >
-        <body className="transition-colors duration-500 ">
-          <DefaultLayout>{children}</DefaultLayout>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+			<ClerkProvider>
+				<html
+					lang="en"
+					className={cn(
+						GeistSans.className,
+						GeistMono.className,
+						outfit.className,
+						// `${inter.variable} ${caveat.variable}`,
+						"antialiased",
+						"h-screen-dvh w-screen-dvw scrollbar-gutter overflow-x-hidden",
+					)}
+				>
+					<body className="transition-colors duration-500 ">
+						<DefaultLayout>{children}</DefaultLayout>
+					</body>
+				</html>
+			</ClerkProvider>
+		);
 }
