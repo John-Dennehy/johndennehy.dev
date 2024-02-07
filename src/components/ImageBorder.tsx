738a3c60ba72287c67ff14e89c4d3d205c6a0146
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-07 17:46:48 +0000

    Refactor ImageBorder component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/ImageBorder.tsx
+++ src/components/ImageBorder.tsx
@@ -5,10 +5,24 @@ type ImageBorderProps = {
 };
 export default function ImageBorder({ size = 320 }: ImageBorderProps) {
 	return (
-		<div className=" relative content-center align-middle items-center h-80 w-80 drop-shadow-md"  >
-			<Image src="/profile-square.jpg" alt="John Dennehy" width={size} height={size} className="absolute rounded-full z-10" />
-			<div className={`absolute h-80 w-80 rounded-full border-l-3 border-b-2 border-brandPrimary z-20`} />
-			<div className={`absolute h-80 w-80 rounded-full border-t-6 border-r-4 border-brandAccent z-20`} />
+		<div className=" relative content-center align-middle items-center h-80 w-80 drop-shadow-md">
+			<Image
+				src="/profile-square.jpg"
+				alt="John Dennehy"
+				width={size}
+				height={size}
+				className="absolute rounded-full z-10"
+			/>
+			<div
+				className={
+					"absolute h-80 w-80 rounded-full border-l-3 border-b-2 border-brandPrimary z-20"
+				}
+			/>
+			<div
+				className={
+					"absolute h-80 w-80 rounded-full border-t-6 border-r-4 border-brandAccent z-20"
+				}
+			/>
 		</div>
 	);
 }
