a6e70114de2a747c8f9a9815497ec00c6871a608
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-07 20:22:50 +0000

    Refactor DashboardPage component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,19 +1,17 @@
+
 type DashboardPageProps = {
 	params: { id: string };
 };
 
-export default function DashboardPage(props: DashboardPageProps) {
+export default function DashboardPage({ params }: DashboardPageProps) {
 	return (
 		<div>
 			<h1>Dashboard</h1>
 			<p>Dashboard</p>
-			{props.params.id}
+			{params.id}
 			{/* create form to add roles to database */}
 
 			{/* form to add skills to database */}
-
-
-
 		</div>
 	);
 } 
