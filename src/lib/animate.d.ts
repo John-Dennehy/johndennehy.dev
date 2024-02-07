7eee0ffc8695f8c0bc34e6c0cf15f6cc598b2d5e
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-07 19:41:51 +0000

    Remove placeholder type animate.d.ts file
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/lib/animate.d.ts
+++
@@ -1,9 +0,0 @@
-// TODO fix while waiting for https://github.com/jamiebuilds/tailwindcss-animate/pull/36
-
-declare module 'tailwindcss-animate' {
-	import type { Plugin } from "tailwindcss";
-	const plugin: Plugin<{}>;
-	export default plugin;
-}
-
-
