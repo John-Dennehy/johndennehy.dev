22240f3bfe91ba13f41b06648a0ab7edd409a810
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-07 20:23:31 +0000

    Commented out Brand Logo image in Branding component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -6,7 +6,7 @@ export default function Branding({ name }: { name: string }) {
 	return (
 		<Link href="/">
 			<div className="flex flex-shrink-0 gap-4 items-center h-full mr-4">
-				<Image src={Logo} height={48} width={48} alt="Brand Logo" />
+				{/* <Image src={Logo} height={48} width={48} alt="Brand Logo" /> */}
 
 				<h1 className="text-xl font-sans font-semibold  tracking-tight align-middle">
 					{name}
