aa5d31728438bcb81b0a35c36d34f3daca022237
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-08 09:46:32 +0000

    Refactor Header component and remove unnecessary code
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,88 +1,42 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import NavLinks from "../Navbar/NavLinks";
 import Link from "next/link";
-import Logo from "@/public/jd-logo.svg";
-import Image from "next/image";
-
-import { SlMenu } from "react-icons/sl";
-
-import { useState } from "react";
-import { cn } from "@/lib/utils";
+import NavLinks from "../Navbar/NavLinks";
 
 const TITLE = "John Dennehy";
 
 export const navLinks = [
-  { href: "/", label: "Home" },
-  { href: "/blog", label: "Blog" },
-  { href: "/projects", label: "Projects" },
-  { href: "/experience", label: "Experience" },
+	{ href: "/", label: "Home" },
+	{ href: "/blog", label: "Blog" },
+	{ href: "/projects", label: "Projects" },
+	{ href: "/experience", label: "Experience" },
 ];
 
-export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  return (
-    <header
-      className="
-      sticky left-0	right-0 top-0
-      z-50
-				shadow-sm
-				backdrop-blur-lg
-				transition-colors
-        print:hidden
-			"
-    >
-      <div className="container mx-auto flex flex-col  p-4 md:grid md:grid-cols-[1fr_1fr_1fr]">
-        <div className="left flex flex-row items-center justify-between">
-          <div className="mr-4 flex h-full flex-shrink-0 items-center gap-4">
-            <Link href="/">
-              <Image src={Logo} height={48} width={48} alt="Brand Logo" />
-            </Link>
-            <Link href="/">
-              <h1 className="align-middle font-sans text-xl  font-semibold tracking-tight">
-                {TITLE}
-              </h1>
-            </Link>
-          </div>
-          <HamburgerMenu
-            onClick={() => {
-              setIsMenuOpen(!isMenuOpen);
-            }}
-          />
-        </div>
-        <div className="right-container col-start-2 col-end-4 grid md:grid-cols-[1fr_1fr]">
-          <div
-            className={cn(
-              "middle z-40 flex flex-row overflow-hidden transition-all duration-300 ease-in-out",
-              "justify-self-center",
-              isMenuOpen ? "max-h-screen" : ""
-            )}
-          >
-            <NavLinks navLinks={navLinks} />
-          </div>
-          <div className="right flex flex-row-reverse items-center gap-4">
-            <UserButton />
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+function JohnDennehy() {
+	return (
+		<Link href="/">
+			<h1 className="align-middle text-3xl font-sans font-semibold tracking-tight py-2">
+				{TITLE}
+			</h1>
+		</Link>
+	);
 }
 
-type HamburgerMenuProps = {
-  onClick: () => void;
-};
+export default function Header() {
+	return (
+		<header className="sticky left-0	right-0 top-0 z-50 bg-white shadow-sm	transition-colors print:hidden ">
+			<div className="container mx-auto ">
+				<div className="flex flex-col md:flex-row  items-center md:justify-between">
+					<div className="left">
+						<JohnDennehy />
+					</div>
 
-function HamburgerMenu({ onClick }: HamburgerMenuProps) {
-  const handleClick = () => {
-    onClick();
-  };
-  // return hamburger menu for mobile using lucide icons
-		return (
-			<button type="button" onClick={handleClick}>
-				<SlMenu />
-			</button>
-		);
+					<div className="right">
+						<NavLinks navLinks={navLinks} />
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
