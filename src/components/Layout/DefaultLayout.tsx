d1ad2cd98cfc93e7db813cc4080ebc3946db4f5f
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-08 09:45:55 +0000

    Refactor DefaultLayout component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/Layout/DefaultLayout.tsx
+++ src/components/Layout/DefaultLayout.tsx
@@ -1,5 +1,5 @@
-import Header from "./Header";
 import Footer from "./Footer";
+import Header from "./Header";
 
 
 type DefaultLayoutProps = {
@@ -8,14 +8,14 @@ type DefaultLayoutProps = {
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
 	return (
-    <div className="grid h-screen grid-rows-pancake ">
-      <Header />
-      <main className="main bg-brandBgLight text-brandText transition-colors duration-500">
-        <div className="container mx-auto print:mx-0">
-          <div className="h-full p-8 print:p-0">{children}</div>
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
+		<div className="grid h-screen grid-rows-pancake">
+			<Header />
+			<main className="main bg-brandBgLight text-brandText transition-colors duration-500">
+				<div className="container mx-auto print:mx-0">
+					<div className="h-full p-8 print:p-0">{children}</div>
+				</div>
+			</main>
+			<Footer />
+		</div>
+	);
 }
